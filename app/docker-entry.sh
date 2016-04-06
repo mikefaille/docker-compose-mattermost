9c7f7149413ba255c1fc912957abf81431b61022
Michaël Faille <michael@faille.io>
2016-04-06 17:42:28 -0400

    Refactoring to use etcd and docker-compose v2

--- app/docker-entry.sh
+++ app/docker-entry.sh
@@ -1,23 +1,36 @@
 #!/bin/bash
-config=/mattermost/config/config.json
+
+# DB_IP=$(getent ahosts db | head -n 1 | cut -f 1 -d " " )
+# ETCD_IP=$(getent ahosts etcd | head -n 1 | cut -f 1 -d " " )
+
 echo -ne "Configure database connection..."
-if [ ! -f $config ]
-then
-    cp /config.template.json $config
-    sed -Ei "s/DB_PORT/$DB_PORT_5432_TCP_PORT/" $config
-    echo OK
-else
-    echo SKIP
-fi
+
+cd /data/templates; confd -onetime -backend etcd -node http://etcd:4001
+
+
+# if [ ! -f $config ]
+# then
+#     cp /config.template.json $config
+#     sed -Ei "s/DB_PORT/$DB_PORT_5432_TCP_PORT" $config
+#     echo OK
+# else
+#     echo SKIP
+# fi
 
 echo "Wait until database is ready..."
-until nc -z db $DB_PORT_5432_TCP_PORT
-do
-    sleep 1
-done
 
-# Wait to avoid "panic: Failed to open sql connection pq: the database system is starting up"
-sleep 1
+# DB_IP=$(getent ahosts db | head -n 1 | cut -f 1 -d " " )
+#DB_PORT_5432_TCP_PORT=5432
+</dev/tcp/db/$DB_PORT_5432_TCP_PORT
+if [ "$?" -ne 0 ]; then
+    echo "Connection to $SERVER on port $PORT failed"
+    sleep 1
+    exit 1
+else
+    echo "Connection to $SERVER on port $PORT succeeded"
+    exit 0
+fi
+
 
 echo "Starting platform"
 cd /mattermost/bin
